a7aca426115a93c179ac05dfebcd8d089feee81a
Joe <51638822+tso996@users.noreply.github.com>
2022-06-12 19:52:34 +0100

    launch sdl2 window

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,32 @@
 
 int main(){
 
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window *window = SDL_CreateWindow("title", SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+    SDL_Event e;
+    bool quit = false;
+    //https://stackoverflow.com/a/41044089/11601765
+    while (!quit){
+        while (SDL_PollEvent(&e)){
+            if (e.type == SDL_QUIT){
+                quit = true;
+            }
+            if (e.type == SDL_KEYDOWN){
+                quit = true;
+            }
+            if (e.type == SDL_MOUSEBUTTONDOWN){
+                quit = true;
+            }
+        }
+    }
+    //===========================The above code is need to make this work with the delay
+    SDL_Delay(1000);
     std::cout<<"SDL is loaded!!"<<std::endl;
+
 
     return 0;
 }
